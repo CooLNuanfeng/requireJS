0938b882adb5c9335a1d5e34a95b7c74646a46a4
CooLNuanfeng <1634898698@qq.com>
2015-05-03 19:38:01 +0800

    2015-05-03

--- js/dialog.js
+++ js/dialog.js
@@ -6,8 +6,13 @@ define(['jquery'],function($){
 			height: 300,
 			y : 50,
 			content : '',
+			skinClassName : null,
+			closeBtn : true,
+			handerClose : null,
+			confirmText : '确定',
 			handerConfirm : null,
-			handerClose : null
+			mask : true,
+			drag : false
 		};
 	}
 
@@ -17,30 +22,77 @@ define(['jquery'],function($){
 
 			var config = $.extend(this.config,cfg);
 			
-			var $dialogBox = $('<div class="win_dialogBox">'+config.content+'</div>');
+			
+			var $dialogBox = $('<div class="win_dialogBox">\
+									<div class="win_dialogHead">'+config.title+'</div>\
+									<div class="win_dialogBody">'+config.content+'</div>\
+								</div>');
 				$dialogBox.appendTo('body');
-			var $dialog_btn = $('<input type="button" class="win_dialogConfirm" value="确定"/>');
-			var $dialog_close = $('<div class="win_dialogClosed">×</div>');
+			var $dialog_btn = $('<div class="win_dialogConfirm">'+config.confirmText+'</div>');
+			
 				$dialog_btn.appendTo($dialogBox);
-				$dialog_close.appendTo($dialogBox);
+				
 
+			if(config.closeBtn){
+				var $dialog_close = $('<div class="win_dialogClosed">×</div>');
+				$dialog_close.appendTo($dialogBox);
 				//关闭按钮
 				$dialog_close.click(function() {
 					config.handerClose && config.handerClose();
 					$dialogBox.remove();
+					if( config.mask ){
+						 $mask.remove();
+					}
 				});
-				//确认按钮
-				$dialog_btn.click(function() {
-					config.handerConfirm && config.handerConfirm();
-					$dialogBox.remove();
-				});
+			}
+				
+			//确认按钮
+			$dialog_btn.click(function() {
+				config.handerConfirm && config.handerConfirm();
+				$dialogBox.remove();
+				if( config.mask ){
+					 $mask.remove();
+				}
+			});
 
-				$dialogBox.css({
-					width : config.width,
-					height : config.height,
-					left: config.x || ($(window).innerWidth() - config.width)/2+'px',
-					top : config.y || ($(window).innerHeight() - config.height)/2+'px'
+			$dialogBox.css({
+				width : config.width,
+				height : config.height,
+				left: config.x || ($(window).innerWidth() - config.width)/2+'px',
+				top : config.y || ($(window).innerHeight() - config.height)/2+'px'
+			});
+
+			//遮罩层
+			if(config.mask){
+				var $mask = $('<div class="win_mask"></div>');
+				$('body').append($mask);
+			}
+
+			//拖动
+			if(config.drag){
+				$('.win_dialogHead').css('cursor','all-scroll');
+				$('.win_dialogHead').on('mousedown',function(ev){
+					var disX = ev.pageX - $(this).offset().left;
+					var disY = ev.pageY - $(this).offset().top;
+
+					$(document).on('mousemove',function(ev){
+						$dialogBox.css({
+							left : ev.pageX - disX,
+							top : ev.pageY - disY
+						});
+					});
+					$(document).on('mouseup',function(){
+						$(document).off('mousemove');
+						$(document).off('mouseup');
+					});
+
+					return false;
 				});
+			}
+			//换肤
+			if(config.skinClassName){
+				$dialogBox.addClass(config.skinClassName);
+			}
 		},
 
 		confirm : function(){
--- js/main.js
+++ js/main.js
@@ -8,10 +8,12 @@ require(['jquery','dialog'],function($,D){
 	var dialog = new D.Dialog();
 	$('#dialog').click(function() {
 		dialog.alert({
+			title : '标题',
 			y:100,
 			content : 'Hello dialog is born',
 			width : 400,
 			height : 300,
+			//drag : true,
 			handerConfirm : function(){
 				alert('you clicked sure');
 			},
